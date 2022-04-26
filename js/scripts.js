d146c6dee6f4c630c915cc489ee0a4417da5cf32
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-04-26 15:57:50 -0700

    Update scripts.js
    created variable pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
-alert("hellow world");
-let favoriteFood = "pizza";
-document.write(favoriteFood);
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['grass', 'poison'],
+    abilities:['Chlorophyll', 'Overgrow']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['fire'],
+    abilities:['Blaze', 'Solar-power']
+  },
+  {
+    name: 'Wartortle',
+    height: 1,
+    types: ['water'],
+    abilities:['Rain-dish', 'Torrent']
+  },
+  {
+    name: 'Weedle',
+    height: 0.3,
+    types: ['bug', 'poison'],
+    abilities:['Shield-dust', 'Run-away']
+  }
+];
