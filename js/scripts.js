f80be93e4da16f6a0cec68cb101fef408a867535
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-04-27 16:57:44 -0700

    Update scripts.js
    conditional statement to render pokemon name and height and to check if height is >1

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,49 @@ let pokemonList = [{
     name: 'Bulbasaur',
     height: 0.7,
     types: ['grass', 'poison'],
-    abilities:['Chlorophyll', 'Overgrow']
+    abilities: ['Chlorophyll', 'Overgrow']
   },
   {
     name: 'Charmander',
     height: 0.6,
     types: ['fire'],
-    abilities:['Blaze', 'Solar-power']
+    abilities: ['Blaze', 'Solar-power']
   },
   {
     name: 'Wartortle',
-    height: 1,
+    height: 1.5,
     types: ['water'],
-    abilities:['Rain-dish', 'Torrent']
+    abilities: ['Rain-dish', 'Torrent']
   },
   {
     name: 'Weedle',
     height: 0.3,
     types: ['bug', 'poison'],
-    abilities:['Shield-dust', 'Run-away']
+    abilities: ['Shield-dust', 'Run-away']
   }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  // create li element
+  let list = document.createElement("li");
+
+  // declare the variables
+  let pokemon;
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+  let message = 'Wow, thats Big!';
+
+  // conditional statement
+  if (pokemonList[i].height > 1) {
+    pokemon = document.createTextNode(pokemonName + ' (height:' + pokemonHeight + ')' + ' -' + message);
+  } else {
+    pokemon = document.createTextNode(pokemonName + ' (height:' + pokemonHeight + ')');
+  }
+
+  // append text to li ele
+  list.appendChild(pokemon);
+  let element = document.getElementById("new");
+
+  //append li ele to ul element
+  element.appendChild(list);
+}
